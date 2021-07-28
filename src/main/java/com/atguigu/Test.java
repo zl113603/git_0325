5acc490135908646ab3d59c8a9db5bc0bc1b513f
zl113603 <zllhm@foxmail.com>
2021-07-29 00:49:08 +0800

    mastercommit

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("sa da");
         System.out.println("hello");
+        System.out.println("sada s");
     }
 }
